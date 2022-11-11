022daf5d93176cecd5d7e46402c6f58dcb531fc1
Anthony Nguyen <100966897+anthonyvnguyen@users.noreply.github.com>
2022-11-10 19:52:06 -0500

    Add files via upload

---
+++ cancerdetection/src/components/AboutOurModel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './AboutOurModel.css';
+
+export default function AboutOurModel() {
+    return(
+
+        <div class = "MESSAGE">
+            <svg>
+                <rect class = "messageBox">
+                    <h2 class = "randomText">Hi</h2>
+                </rect>
+            </svg>
+        </div>
+    );
+}
