349511efe7d385f7b5e1b25ceb67f3c4c0a9cf19
ramakhabbaz <98370927+ramakhabbaz@users.noreply.github.com>
2022-12-02 18:07:55 -0600

    file uploader

--- cancerdetection/src/App.js
+++ cancerdetection/src/App.js
@@ -4,15 +4,18 @@ import {Routes,Route} from 'react-router-dom';
 import Home from './components/Home';
 import RecentNews from './components/RecentNews';
 import AboutOurModel from './components/AboutOurModel';
+import { FileUploader } from './components/FileUploader';
 
 function App() {
   return (
-    <Routes>
-      <Route path = '/' element = {<Home />}/>;
-      <Route path = 'recentnews' element = {<RecentNews />}/>;
-      <Route path = 'aboutourmodel' element = {<AboutOurModel />}/>;
+    <><div className="App">
+          <FileUploader />
+      </div><Routes>
+              <Route path='/' element={<Home />} />;
+              <Route path='recentnews' element={<RecentNews />} />;
+              <Route path='aboutourmodel' element={<AboutOurModel />} />;
 
-    </Routes>
+          </Routes></>
   );
 }
 
