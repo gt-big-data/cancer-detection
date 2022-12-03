fe212b50cfc40d4675dd6233c0cb7e6d47ff1bf0
ramakhabbaz <98370927+ramakhabbaz@users.noreply.github.com>
2022-12-02 18:11:23 -0600

    file uploader

---
+++ cancerdetection/src/components/server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public')
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+});
+
+const upload = multer({storage}).array('file')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            return res.status(500).json(err)
+        }
+        return res.status(200).send(req.file)
+    })
+});
+
+app.listen(8000, () => {
+    console.log('App is running on port 8000')
+});
