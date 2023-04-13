8661d6d934aee78ef92f68423594a7c45aad49d6
tuhm6 <tuhm7@gatech.edu>
2023-04-13 18:15:26 -0400

    updated nav bar

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -9,26 +9,32 @@ import {
   Code,
   Grid,
   GridItem,
-  Image
+  Image,
 } from '@chakra-ui/react';
 //Colors that we will use
-import { theme }from './components/themeFile.js';
+import { theme } from './components/themeFile.js';
 import Navbar from './components/navbar.js';
 import Home from './pages/homePage.js';
 import AboutUsPage from './pages/ourTeam.js';
-
-
-
+// import { HashLink as HLink } from 'react-router-hash-link';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <ChakraProvider theme={theme}>
-        <Navbar />
-        <Home />
-        <AboutUsPage />
-      </ChakraProvider>
-    </div>
+    <BrowserRouter>
+
+        <ChakraProvider theme={theme}>
+          <Navbar />
+          <Home />
+          <AboutUsPage />
+        </ChakraProvider>
+
+    </BrowserRouter>
   );
 }
 
--- client/my-app/src/components/navbar.js
+++ client/my-app/src/components/navbar.js
@@ -1,18 +1,23 @@
 import { Box, Link, HStack, Center, VStack, Flex } from '@chakra-ui/react';
 import { ChakraProvider } from '@chakra-ui/react';
-import { theme }from './themeFile.js';
+import { BrowserRouter } from 'react-router-dom';
+import { theme } from './themeFile.js';
+import { HashLink as HLink } from 'react-router-hash-link';
 
 export default function Navbar() {
   return (
+    <nav>
     <ChakraProvider theme={theme}>
       <Center w="100%" bg="brand.300">
-        <HStack h="50px" spacing="100px" as='b'>
-          <Link href="https://google.com" >Home</Link>
-          <Link href="https://gmail.com">Our Team</Link>
+        <HStack h="50px" spacing="100px" as="b">
+          <Link href="https://google.com">Home</Link>
+          <HLink to='#ourteam' smooth>Our Team</HLink>
           <Link href="https://drive.google.com">Our Model</Link>
           <Link href="https://google.com/maps">Filler2</Link>
+
         </HStack>
       </Center>
     </ChakraProvider>
+    </nav>
   );
 }
